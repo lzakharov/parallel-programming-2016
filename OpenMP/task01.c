64aee0ba4466fbf6c3cf92148d24898afb62d130
Lev Zakharov <l.j.zakharov@gmail.com>
2017-02-17 23:14:51 +0300

    Update task01.c

--- OpenMP/task01.c
+++ OpenMP/task01.c
@@ -3,7 +3,7 @@
 
 int main() 
 {
-    #pragma omp parallel
+    #pragma omp parallel num_threads(8)
     {
         printf("Hello world from thread %d of %d\n", 
             omp_get_thread_num(), omp_get_num_threads());
