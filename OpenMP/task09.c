88e9d1eb4e66d3bad4ca9c278eb77efac3e960c2
Lev Zakharov <l.j.zakharov@gmail.com>
2017-02-18 23:30:04 +0300

    Add task09.c

---
+++ OpenMP/task09.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <omp.h>
+#include "time.h"
+#include "stdlib.h"
+
+#define N 1000
+#define M 1000
+
+int main() 
+{
+    int a[N][M];
+    int v[M];
+    int res[N];
+    double start;
+    double end;
+    srand(time(NULL));
+
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < M; j++)
+        {
+            a[i][j] = rand() / (N * M);
+        }
+    }
+
+    for (int i = 0; i < M; i++)
+    {
+        v[i] = rand() / (N * M);
+    }
+
+    start = omp_get_wtime();
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) {
+            res[i] += a[i][j] * v[j];
+        }
+    }
+    end = omp_get_wtime();
+
+    printf("Single thread: %.6f\n", end - start);
+
+    start = omp_get_wtime();
+    #pragma omp parallel for schedule(auto)
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) {
+            res[i] += a[i][j] * v[j];
+        }
+    }
+    end = omp_get_wtime();
+
+    printf("Parallel: %.6f\n", end - start);
+
+    return 0;
+}
